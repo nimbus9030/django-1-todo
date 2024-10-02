193f81ea0a76038458d953ef2c7c0b50d5f4d8c0
nimbus9030 <nimbus9030@gmail.com>
2024-10-02 14:35:57 +0900

    完了

--- todo/urls.py
+++ todo/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.Index, name="index"),
     path("add", views.Add, name="add"),
+    path("<int:todo_id>/delete", views.Delete, name="delete"),
+    path("<int:todo_id>/update", views.Update, name="update"),
+
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from .forms import TodoModelForm
+from .models import Todo
 
 def Index(request):
     form = TodoModelForm()
+    todo_list = Todo.objects.all()
+    print( todo_list )
 
-    return render( request, "todo/index.html", {"form" : form} ) 
+    return render( request, "todo/index.html", {"form" : form, "list" : todo_list} ) 
     # return HttpResponse( "hello world" )
 
 
@@ -16,26 +19,21 @@ def Add(request):
             form.save()
 
             return redirect("index")
-        
-# def add(request):
-#     title = request.POST['title']
-#     Todo.objects.create(title=title)
 
-#     return redirect('todos:index')
+def Delete(request, todo_id):
+    todo = get_object_or_404(Todo, pk=todo_id)
+    todo.delete()
 
-# def delete(request, todo_id):
-#     todo = get_object_or_404(Todo, pk=todo_id)
-#     todo.delete()
+    return redirect('index')
 
-#     return redirect('todos:index')
 
-# def update(request, todo_id):
-#     todo = get_object_or_404(Todo, pk=todo_id)
-#     isCompleted = request.POST.get('isCompleted', False)
-#     if isCompleted == 'on':
-#         isCompleted = True
+def Update(request, todo_id):
+    todo = get_object_or_404(Todo, pk=todo_id)
+    isCompleted = request.POST.get('isCompleted', False)
+    if isCompleted == 'on':
+        isCompleted = True
     
-#     todo.isCompleted = isCompleted
+    todo.isCompleted = isCompleted
 
-#     todo.save()
-#     return redirect('todos:index')
+    todo.save()
+    return redirect('index')
